ce442950e0793d3e6e57bb3c6c168db92c4ca67c
Benjamin <ben.d.maryland@gmail.com>
2021-08-20 16:58:34 -0400

    Assignment Completed

---
+++ index.js
@@ -0,0 +1,31 @@
+function add (number1,number2) {
+    return number1 + number2
+}
+function subtract (number1,number2) {
+    return number1-number2
+}
+function multiply (number1,number2) {
+    return number1*number2
+}
+function divide (number1,number2) {
+    return number1/number2
+}
+function increment(number1){
+return number1 + 1
+}
+function decrement(number1){
+    return number1 - 1
+ }
+function  makeInt(number1) {
+    return number1
+}
+function  makeInt(number1) {
+    return parseFloat (number1)
+}
+function  preserveDecimal(number1){
+    return number1
+}
+function  preserveDecimal(number1){
+    return parseFloat(number1)
+
+}
